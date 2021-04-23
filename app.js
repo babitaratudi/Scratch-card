70cca69c325129399c44044019c511956557a748
babitaratudi <babita.r@somaiya.edu>
2021-04-23 23:18:27 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-//var num = Math.floor(Math.random() * 4) + 1;
+/*var num = Math.floor(Math.random() * 9) + 1;
+
 
 $(".card").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  //bg: `Images/Gpay_Card ${num}.jpg`, // Background (image path or hex color).
+  bg: `assets/img/items/${num}.jpg`, // Background (image path or hex color).
   fg: `Images/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
+
+console.log(bg)*/
+
+function select(self){
+  var id=self.getAttribute("data-id")
+  console.log(id)
+  var num = Math.floor(Math.random() * 9) + 1;  
+  document.getElementById(id).src=`assets/img/items/${num}.jpg`
+  document.getElementById(id).innerHTML=`"Hurrey! You Won 10% Discount"`
+}
 
 
